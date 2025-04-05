554aca9a63eee212a57ed4f72895c43662f2eeb2
Shivam Ugale <141569702+Shivam21512@users.noreply.github.com>
2025-04-05 23:54:13 +0530

    Update Spinner.jsx

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -12,34 +12,3 @@ const Spinner = () => {
 
 
 export default Spinner;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import "./Spinner.css";
-
-// const Spinner = () => {
-//   return (
-//     <div className='grid justify-items-center w-[100%] h-screen'>
-//       <div className='spinner'></div>
-//     </div>
-//   )
-// }
-
-// export default Spinner
